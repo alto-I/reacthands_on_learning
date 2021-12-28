f1a87543a680f3ce5cd074252d794da1917ad6c6
alto-I <blmaldiy@gmail.com>
2021-12-28 15:19:39 +0900

    6章まで

--- ch6/color-data/src/ColorList.js
+++ ch6/color-data/src/ColorList.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import { useContext } from 'react/cjs/react.production.min'
+import { ColorContext } from '.'
 import Color from './Color'
 
-export default function ColorList({
-  colors = [],
-  onRemoveColor = (f) => f,
-  onRateColor = (f) => f,
-}) {
+export default function ColorList() {
+  const { colors } = useContext(ColorContext)
   if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>
   return (
     <div>
       {colors.map((color) => (
-        <Color key={color.id} {...color} onRemove={onRemoveColor} onRate={onRateColor} />
+        <Color key={color.id} {...color} />
       ))}
     </div>
   )
--- ch6/color-data/src/index.js
+++ ch6/color-data/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from 'react'
+import colors from './color-data.json'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+export const ColorContext = createContext()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ColorContext.Provider value={{ colors }}>
+      <App />
+    </ColorContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
